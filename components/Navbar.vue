fd938b0e5371aa22c18bbbf55fe5f1e288ff2c8e
Mavlonyusuf <mavlonjonyusupov1@gmail.com>
2025-12-29 15:33:49 +0500

    navbar qoshildi va fix qilindi

---
+++ components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <ul>
+            <li>Link1</li>
+            <li>Link1</li>
+            <li>Link1</li>
+            <li>Link1</li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
